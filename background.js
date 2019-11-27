00bf291caf3b188dab080fbd69c6ab3b4d0aeccc
Mengjun Liu <mengjun_liu@126.com>
2019-11-27 22:57:57 +0800

    do not enable tripleClickEnabled by default

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({'tripleClickEnabled': true}, function() {
-    console.log('triple click to pop content is enabled.');
-  });
+  // chrome.storage.sync.set({'tripleClickEnabled': true}, function() {
+  //   console.log('triple click to pop content is enabled.');
+  // });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
